40a7a935209cab505c550857c545d5fdc7ab1c20
jonataslr <jlr@cin.ufpe.br>
2014-02-24 13:16:07 -0300

    Professor+Artigo
    -Adicionando tudo em Artigo
    -Criação da entidade Professor
    -Criação do Professor

---
+++ src/br/ufpe/cin/entidades/Professor.java
@@ -0,0 +1,79 @@
+package br.ufpe.cin.entidades;
+
+import java.util.List;
+
+public class Professor {
+	
+	private String nomeCompleto;
+	private String nomeCitacoes;
+	private String nacionalidade;
+	private String paisNascimento;
+	private String UFNascimento;
+	private String cidadeNascimento;
+	private List<Artigo> artigos;
+	
+	
+	public List<Artigo> getArtigos() {
+		return artigos;
+	}
+
+
+	public void setArtigos(List<Artigo> artigos) {
+		this.artigos = artigos;
+	}
+
+	public Professor(){
+		
+	}
+	
+	public Professor(String nomeCompleto, String nomeCitacoes,
+			String nacionalidade, String paisNascimento, String uFNascimento,
+			String cidadeNascimento) {
+		super();
+		this.nomeCompleto = nomeCompleto;
+		this.nomeCitacoes = nomeCitacoes;
+		this.nacionalidade = nacionalidade;
+		this.paisNascimento = paisNascimento;
+		UFNascimento = uFNascimento;
+		this.cidadeNascimento = cidadeNascimento;
+	}
+	
+	
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+	public String getNomeCitacoes() {
+		return nomeCitacoes;
+	}
+	public void setNomeCitacoes(String nomeCitacoes) {
+		this.nomeCitacoes = nomeCitacoes;
+	}
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+	public void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+	public String getPaisNascimento() {
+		return paisNascimento;
+	}
+	public void setPaisNascimento(String paisNascimento) {
+		this.paisNascimento = paisNascimento;
+	}
+	public String getUFNascimento() {
+		return UFNascimento;
+	}
+	public void setUFNascimento(String uFNascimento) {
+		UFNascimento = uFNascimento;
+	}
+	public String getCidadeNascimento() {
+		return cidadeNascimento;
+	}
+	public void setCidadeNascimento(String cidadeNascimento) {
+		this.cidadeNascimento = cidadeNascimento;
+	}
+	
+}
--- src/br/ufpe/cin/leitor/LeitorXML.java
+++ src/br/ufpe/cin/leitor/LeitorXML.java
@@ -13,6 +13,8 @@ import org.jdom2.input.SAXBuilder;
 
 import br.ufpe.cin.entidades.Artigo;
 import br.ufpe.cin.entidades.Autor;
+import br.ufpe.cin.entidades.PalavrasChave;
+import br.ufpe.cin.entidades.Professor;
 
 public class LeitorXML {
 
@@ -21,6 +23,7 @@ public class LeitorXML {
 			throws IOException, JDOMException{
 
 		List<Artigo> listaArtigos = new ArrayList<Artigo>(); //lista com os artigos
+		Professor professor = new Professor();
 		
 		SAXBuilder sb = new SAXBuilder();
 
@@ -34,6 +37,14 @@ public class LeitorXML {
 		while(i.hasNext()){
 			Element partes = (Element) i.next();
 
+			if(partes.getName().equals("DADOS-GERAIS")){
+				
+				professor = new Professor(partes.getAttributeValue("NOME-COMPLETO"), partes.getAttributeValue("NOME-EM-CITACOES-BIBLIOGRAFICAS"), partes.getAttributeValue("NACIONALIDADE"), 
+						partes.getAttributeValue("PAIS-DE-NASCIMENTO"), partes.getAttributeValue("UF-NASCIMENTO"), partes.getAttributeValue("CIDADE-NASCIMENTO"));
+				
+			}
+
+
 			//Pega as producoes bibliograficas
 			if(partes.getName().equals("PRODUCAO-BIBLIOGRAFICA")){
 				List producoesBibliograficas = partes.getChildren();
@@ -42,43 +53,67 @@ public class LeitorXML {
 
 				while(iPB.hasNext()){
 					Element producaoBibliografica = (Element) iPB.next();
-					
+
 					//Pega a parte de artigos publicados
 					if(producaoBibliografica.getName().equals("ARTIGOS-PUBLICADOS")){
 						List artigosPublicados = producaoBibliografica.getChildren();
-						
+
 						Iterator iAP = artigosPublicados.iterator();
-						
+
 						while(iAP.hasNext()){
 							Element artigoPublicado = (Element) iAP.next();
-							
+
 							//pega os atributos do artigo
 							if(artigoPublicado.getName().equals("ARTIGO-PUBLICADO")){
 								List artigos = artigoPublicado.getChildren();
-								
+
 								Iterator iArtigo = artigos.iterator();
-								
+
 								//Cria lista de Autores
 								List<Autor> autores = new ArrayList<Autor>();
 								Artigo novoArtigo = new Artigo();
-								
+
 								//enqaunto houver atributos dentro de artigo
 								while(iArtigo.hasNext()){
 									Element artigo = (Element) iArtigo.next();
-									
+
 									if(artigo.getName().equals("DADOS-BASICOS-DO-ARTIGO")){
+										/*
 										System.out.println("Titulo: " + artigo.getAttributeValue("TITULO-DO-ARTIGO"));
 										System.out.println("País: " + artigo.getAttributeValue("PAIS-DE-PUBLICACAO"));
 										System.out.println("Idioma: " + artigo.getAttributeValue("IDIOMA"));
 										System.out.println("Meio de Divulgação: " + artigo.getAttributeValue("MEIO-DE-DIVULGACAO"));
-										
+										 */
+
 										//colocando no objeto Artigo
 										novoArtigo.setTitulo(artigo.getAttributeValue("TITULO-DO-ARTIGO"));
 										novoArtigo.setPais(artigo.getAttributeValue("PAIS-DE-PUBLICACAO"));
 										novoArtigo.setIdioma(artigo.getAttributeValue("IDIOMA"));
 										novoArtigo.setMeioDeDivulgacao(artigo.getAttributeValue("MEIO-DE-DIVULGACAO"));
-										
+
 									}else if(artigo.getName().equals("DETALHAMENTO-DO-ARTIGO")){
+
+										novoArtigo.setTituloPeriodicoRevista(artigo.getAttributeValue("TITULO-DO-PERIODICO-OU-REVISTA"));
+										novoArtigo.setIssn(artigo.getAttributeValue("ISSN"));
+										if(artigo.getAttributeValue("VOLUME").equals("")){
+											novoArtigo.setVolume(0);
+										}else{
+											novoArtigo.setVolume(Integer.parseInt(artigo.getAttributeValue("VOLUME")));
+										}
+										novoArtigo.setFasciculo(artigo.getAttributeValue("FASCICULO"));
+										if(artigo.getAttributeValue("PAGINA-INICIAL").equals("")){
+											novoArtigo.setPagInicial(0);
+										}else{
+											novoArtigo.setPagInicial(Integer.parseInt(artigo.getAttributeValue("PAGINA-INICIAL")));
+										}
+										if(artigo.getAttributeValue("PAGINA-FINAL").equals("")){
+											novoArtigo.setPagFinal(0);
+										}else{
+											novoArtigo.setPagFinal(Integer.parseInt(artigo.getAttributeValue("PAGINA-FINAL")));
+										}
+										novoArtigo.setLocalPublicacao(artigo.getAttributeValue("LOCAL-DE-PUBLICACAO"));
+
+										/*
 										System.out.println("Periódico ou Revista: " + artigo.getAttributeValue("TITULO-DO-PERIODICO-OU-REVISTA"));
 										System.out.println("ISSN: " + artigo.getAttributeValue("ISSN"));
 										System.out.println("Volume: " + artigo.getAttributeValue("VOLUME"));
@@ -86,19 +121,29 @@ public class LeitorXML {
 										System.out.println("Pagina Inicial: " + artigo.getAttributeValue("PAGINA-INICIAL"));
 										System.out.println("Página Final: " + artigo.getAttributeValue("PAGINA-FINAL"));
 										System.out.println("Local de Publicação: " + artigo.getAttributeValue("LOCAL-DE-PUBLICACAO"));
+										 */
 									}else if(artigo.getName().equals("AUTORES")){
-										System.out.println("Nome Completo: " + artigo.getAttributeValue("NOME-COMPLETO-DO-AUTOR"));
-										System.out.println("Nome para citação: " + artigo.getAttributeValue("NOME-PARA-CITACAO"));
+										autores.add(new Autor(artigo.getAttributeValue("NOME-COMPLETO-DO-AUTOR"),artigo.getAttributeValue("NOME-PARA-CITACAO")));
+										//System.out.println("Nome Completo: " + artigo.getAttributeValue("NOME-COMPLETO-DO-AUTOR"));
+										//System.out.println("Nome para citação: " + artigo.getAttributeValue("NOME-PARA-CITACAO"));
 									}else if(artigo.getName().equals("PALAVRAS-CHAVE")){
+
+										novoArtigo.setPalavrasChave(new PalavrasChave(artigo.getAttributeValue("PALAVRA-CHAVE-1"), artigo.getAttributeValue("PALAVRA-CHAVE-2"), 
+												artigo.getAttributeValue("PALAVRA-CHAVE-3"), artigo.getAttributeValue("PALAVRA-CHAVE-4"), artigo.getAttributeValue("PALAVRA-CHAVE-5"), 
+												artigo.getAttributeValue("PALAVRA-CHAVE-6")));
+
+										/*
 										System.out.println("Palavra Chave 1: " + artigo.getAttributeValue("PALAVRA-CHAVE-1"));
 										System.out.println("Palavra Chave 2: " + artigo.getAttributeValue("PALAVRA-CHAVE-2"));
 										System.out.println("Palavra Chave 3: " + artigo.getAttributeValue("PALAVRA-CHAVE-3"));
 										System.out.println("Palavra Chave 4: " + artigo.getAttributeValue("PALAVRA-CHAVE-4"));
 										System.out.println("Palavra Chave 5: " + artigo.getAttributeValue("PALAVRA-CHAVE-5"));
 										System.out.println("Palavra Chave 6: " + artigo.getAttributeValue("PALAVRA-CHAVE-6"));
+										 */
 									}
-									
+
 								}
+								novoArtigo.setAutores(autores);
 								listaArtigos.add(novoArtigo);
 							}
 						}
@@ -109,22 +154,17 @@ public class LeitorXML {
 			}
 
 		}
+		
+		//Finaliza professor com artigos
+		professor.setArtigos(listaArtigos);
+		/*
+		System.out.println(professor.getNomeCompleto());
+		System.out.println(professor.getArtigos().get(0).getTitulo());
+		System.out.println(professor.getArtigos().get(0).getTituloPeriodicoRevista());
+		System.out.println(professor.getArtigos().get(1).getTitulo());
+		System.out.println(professor.getArtigos().get(1).getTituloPeriodicoRevista());
+		*/
 
 	}
 
-	private List criaLista(){
-
-
-
-		return null;
-
-	}
 }
-
-/*
-if(livro.getName().equals("DADOS-BASICOS-DO-LIVRO")){
-	System.out.println("Ano: " + livro.getAttributeValue("ANO"));
-	System.out.println("Título: " + livro.getAttributeValue("TITULO-DO-LIVRO"));
-}else if(livro.getName().equals("AUTORES")){
-	System.out.println("Autor: " + livro.getAttributeValue("NOME-COMPLETO-DO-AUTOR"));
-}*/
